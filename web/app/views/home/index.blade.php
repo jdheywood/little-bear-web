15cec0fb02d157915a62b411c9905b2ab1b3a8f9
James Heywood <jdheywood@yahoo.co.uk>
2015-03-03 07:38:29 +0000

    Tweaks to bring pages in line with new layout

--- web/app/views/home/index.blade.php
+++ web/app/views/home/index.blade.php
@@ -15,49 +15,26 @@
         <div class="row">
             <div class="col-lg-4">
                 <img class="img-circle" src="holder.js/140x140/auto" alt="Generic placeholder image" style="width: 140px; height: 140px;">
-                <h2>Heading</h2>
+                <h2>Project Management</h2>
                 <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.
                     Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
                 <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
             </div>
             <div class="col-lg-4">
                 <img class="img-circle" src="holder.js/140x140/auto" alt="Generic placeholder image" style="width: 140px; height: 140px;">
-                <h2>Heading</h2>
+                <h2>Production</h2>
                 <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur
                     purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
                 <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
             </div>
             <div class="col-lg-4">
                 <img class="img-circle" src="holder.js/140x140/auto" alt="Generic placeholder image" style="width: 140px; height: 140px;">
-                <h2>Heading</h2>
+                <h2>Cakes!</h2>
                 <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper.
                     Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
                 <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
             </div>
         </div>
-
-        <hr class="featurette-divider">
-
-        <div class="row featurette">
-            <div class="col-md-7">
-                <h2 class="featurette-heading">
-                    Uuummmm, this is a tasty carousel! <span class="text-muted">It'll blow your mind.</span>
-                </h2>
-                <p class="lead">Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.</p>
-            </div>
-            <div class="col-md-5">
-                <img class="featurette-image img-responsive" src="holder.js/500x500/auto" alt="500x500"/>
-            </div>
-        </div>
-
-        <h2>No man, I don't eat pork</h2>
-        <p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man. </p>
-
-        <h2>Hold on to your butts</h2>
-        <p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
-
-        <h2>Are you ready for the truth?</h2>
-        <p>Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb. </p>
 
         <hr class="featurette-divider">
 
--- web/app/views/production/index.blade.php
+++ web/app/views/production/index.blade.php
@@ -1,7 +1,61 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: james
- * Date: 03/03/15
- * Time: 06:59
- */
+@section('content')
+
+    <div class="container marketing">
+
+        <hr class="divider">
+
+        <div class="jumbotron">
+            <h1>{{$pageTitle}}</h1>
+            <p class="lead">Bacon ipsum dolor amet venison doner rump landjaeger pancetta, spare ribs flank shankle beef ribs strip steak.
+                Shoulder strip steak prosciutto swine bresaola andouille tri-tip ground round.</p>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading">
+                    Production Management <span class="text-muted">Years of this, all over it.</span>
+                </h2>
+                <p class="lead">Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.</p>
+            </div>
+            <div class="col-md-5">
+                <img class="featurette-image img-responsive" src="holder.js/500x500/auto" alt="500x500"/>
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-5">
+                <img class="featurette-image img-responsive" src="holder.js/500x500/auto" alt="500x500"/>
+            </div>
+            <div class="col-md-7">
+                <h2 class="featurette-heading">
+                    Supplier Coordination <span class="text-muted">I'm adept at herding those cats.</span>
+                </h2>
+                <p class="lead">You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man.</p>
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading">
+                    Something something production <span class="text-muted">I can do all this stuff too, look!</span>
+                </h2>
+                <p class="lead">Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.</p>
+            </div>
+            <div class="col-md-5">
+                <img class="featurette-image img-responsive" src="holder.js/500x500/auto" alt="500x500"/>
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        @include('shared.footer')
+
+    </div>
+
+@stop
